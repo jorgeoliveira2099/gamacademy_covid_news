acd05a00a2830eab3ac2666fb96ba969f729dbb7
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-04-18 20:36:44 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+var array = ['bruce', 'pedroso', 'seu zeca'];
+
+var anos = [1998, 2000, 2002, 2003];
+
+console.log(anos[2]);
+console.log(array[2]);
+
+array.push('jooj');
+console.log(array);
+
+console.log(anos);
+
+console.log(anos.indexOf(2003));
+$(document).ready(function(){
+
+    $('div').css('color', 'blue');
+})
